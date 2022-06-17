4316bb501e015cce39d3950bbbe3c76f50c8096c
BrtXa <DuAnChoiGameThanBi@gmail.com>
2022-06-17 23:10:10 +0700

    add tablesaw library

--- src/test/tableTest.java
+++ src/test/tableTest.java
@@ -1,17 +1,11 @@
 package test;
 
-import tech.tablesaw.api.DoubleColumn;
 import tech.tablesaw.api.Table;
 
 public class tableTest {
 
     public static void main(String[] args) throws Exception {
-        double[] numbers = { 1, 2, 3, 4 };
-        try {
-
-            DoubleColumn nc = DoubleColumn.create("nc", numbers);
-        } catch (Exception e) {
-            System.out.println(e);
-        }
+        Table covidTable = Table.read().csv("assets/data/output_covid_data.csv");
+        System.out.println(covidTable.structure());
     }
 }
