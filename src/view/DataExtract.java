481a991a2ea9cb22792bf6ad76a56b04f0b5542a
btdun2002 <54074236+btdun2002@users.noreply.github.com>
2022-06-19 17:20:02 +0700

    Update DataExtract.java

--- src/view/DataExtract.java
+++ src/view/DataExtract.java
@@ -19,7 +19,7 @@ public class DataExtract extends JFrame {
         this.setSize((int) (ScreenRes.WIDTH * 2 / 3), (int) (ScreenRes.HEIGHT * 2 / 3));
         this.setLocationRelativeTo(null);
         GridBagLayout gbl = new GridBagLayout();
-        this.setLayout(new GridBagLayout());
+        this.setLayout(gbl);
         this.setResizable(false);
 
         // Create dropdown box to select country. Get the column containing all
@@ -34,46 +34,46 @@ public class DataExtract extends JFrame {
         // With the created array, use it as elememnts in JComboBox.
         JComboBox<String> countrySelect = new JComboBox<>(countries);
 
-        JPanel whitePanel = new JPanel();
-        whitePanel.setBackground(Palette.WHITE);
-        gbc.fill = GridBagConstraints.BOTH;
-        gbc.gridx = 0;
-        gbc.gridy = 0;
-        gbc.weightx = 0.25;
-        gbc.weighty = 1;
-        gbc.anchor = GridBagConstraints.FIRST_LINE_START;
-        whitePanel.add(countrySelect);
-        this.add(whitePanel, gbc);
-
-        // Temporary panel to put components.
-        JPanel tempPanel = new JPanel();
-        tempPanel.setBackground(Palette.HUST_RED);
-        gbc.fill = GridBagConstraints.BOTH;
-        gbc.gridx = 1;
-        gbc.gridy = 0;
-        gbc.weightx = 0.75;
-        gbc.weighty = 1;
-        this.add(tempPanel, gbc);
-
-        // // Building essential panels
-        // JPanel countrySelectPanel = new JPanel();
-        // countrySelectPanel.setLayout(new BorderLayout());
-        // countrySelectPanel.add(countrySelect);
-
         // JPanel whitePanel = new JPanel();
-        // whitePanel.setBackground(Color.WHITE);
+        // whitePanel.setBackground(Palette.WHITE);
+        // gbc.fill = GridBagConstraints.BOTH;
+        // gbc.gridx = 0;
+        // gbc.gridy = 0;
+        // gbc.weightx = 0.25;
+        // gbc.weighty = 1;
+        // gbc.anchor = GridBagConstraints.FIRST_LINE_START;
+        // whitePanel.add(countrySelect);
+        // this.add(whitePanel, gbc);
 
-        // JPanel redPanel = new JPanel();
-        // redPanel.setBackground(Color.RED);
+        // // Temporary panel to put components.
+        // JPanel tempPanel = new JPanel();
+        // tempPanel.setBackground(Palette.HUST_RED);
+        // gbc.fill = GridBagConstraints.BOTH;
+        // gbc.gridx = 1;
+        // gbc.gridy = 0;
+        // gbc.weightx = 0.75;
+        // gbc.weighty = 1;
+        // this.add(tempPanel, gbc);
 
-        // // Set up the layout for the JFrame
-        // AddPanel(countrySelectPanel, 0, 0, 1, 1, 0.0, 0.0);
-        // AddPanel(whitePanel, 0, 1, 1, 1, 0.0, 0.0);
-        // AddPanel(redPanel, 1, 0, 2, 2, 0.0, 0.0);
+        // Building essential panels
+        JPanel countrySelectPanel = new JPanel();
+        countrySelectPanel.setLayout(new BorderLayout());
+        countrySelectPanel.add(countrySelect);
 
-        // // Tuning the layout for the JFrame
-        // gbl.columnWeights = new double[] { 0.33, 0.33, 0.66 };
-        // gbl.rowWeights = new double[] { 0.01, 0.99, 1 };
+        JPanel whitePanel = new JPanel();
+        whitePanel.setBackground(Color.WHITE);
+
+        JPanel redPanel = new JPanel();
+        redPanel.setBackground(Color.RED);
+
+        // Set up the layout for the JFrame
+        AddPanel(countrySelectPanel, 0, 0, 1, 1, 0.0, 0.0);
+        AddPanel(whitePanel, 0, 1, 1, 1, 0.0, 0.0);
+        AddPanel(redPanel, 1, 0, 3, 2, 0.0, 0.0);
+
+        // Tuning the layout for the JFrame
+        gbl.columnWeights = new double[] { 0.1, 0.1, 0.9 };
+        gbl.rowWeights = new double[] { 0.01, 0.99, 1 };
     }
 
     public void AddPanel(JPanel panel, int gridx, int gridy, int gridwidth, int gridheight, double weightx,
