7b793f16c9241f83cb9b5a9ae75be9b5bbd83cf6
BrtXa <DuAnChoiGameThanBi@gmail.com>
2022-06-21 00:45:30 +0700

    Added required .jar files for jsplot

--- src/test/tableTest.java
+++ src/test/tableTest.java
@@ -1,22 +1,16 @@
 package test;
 
-import java.util.List;
-
 import tech.tablesaw.api.*;
 import tech.tablesaw.columns.*;
+import tech.tablesaw.plotly.Plot;
+import tech.tablesaw.plotly.api.TimeSeriesPlot;
 
 public class tableTest {
     public static Table covidTable = Table.read().csv("assets/data/output_covid_data.csv");
     public static Column<?> entitiesColumns = covidTable.column("location").unique();
 
     public static void main(String[] args) throws Exception {
-        entitiesColumns.sortAscending();
-        List<?> entites = entitiesColumns.asList();
-        String countries[] = new String[entites.size()];
-        countries = entites.toArray(countries);
-
-        for (int i = 0; i < entites.size(); i++) {
-            System.out.println(countries[i]);
-        }
+        Table vie = covidTable.where(covidTable.stringColumn("location").equalsIgnoreCase("Vietnam"));
+        Plot.show(TimeSeriesPlot.create("Vietnam new cases", vie, "date", "new_cases"));
     }
 }
