f47236d6f9a1affa9e297e0b9bc783b50a8046f6
BrtXa <DuAnChoiGameThanBi@gmail.com>
2022-07-08 01:04:59 +0700

    Add country selector to graph screen.

--- src/view/components/graphFilter/TimeSeriesFilterHolder.java
+++ src/view/components/graphFilter/TimeSeriesFilterHolder.java
@@ -4,12 +4,14 @@ import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
 
+import model.*;
 import view.TimeSeriesFilter;
 import view.Util.Palette;
-import view.components.ColoredButton;
+import view.components.*;
 
 public class TimeSeriesFilterHolder extends JPanel implements ActionListener {
     private GridBagConstraints gbc = new GridBagConstraints();
+    private CheckComboPanel countrySelect;
     private ColoredButton filterBtn = new ColoredButton("Bộ lọc");
 
     public TimeSeriesFilterHolder() {
@@ -17,11 +19,25 @@ public class TimeSeriesFilterHolder extends JPanel implements ActionListener {
         this.setLayout(new GridBagLayout());
         this.setBackground(Palette.WHITE);
 
+        // Create a Combobox with checkbox by calling the CheckComboPanel class with
+        // list of countries obtained from DataExtract class.
+        String[] countries = DataExtract.extractCountry();
+        this.countrySelect = new CheckComboPanel(countries);
+
+        gbc.gridx = 0;
+        gbc.gridy = 0;
+        gbc.gridwidth = 1;
+        gbc.gridheight = 1;
+        gbc.weightx = 0;
+        gbc.weighty = 0;
+        gbc.fill = GridBagConstraints.BOTH;
+        this.add(countrySelect, gbc);
+
         JPanel greenPanel = new JPanel();
         greenPanel.setBackground(Color.GREEN);
 
         gbc.gridx = 0;
-        gbc.gridy = 0;
+        gbc.gridy = 1;
         gbc.gridwidth = 1;
         gbc.gridheight = 1;
         gbc.weightx = 1;
@@ -30,7 +46,7 @@ public class TimeSeriesFilterHolder extends JPanel implements ActionListener {
         this.add(greenPanel, gbc);
 
         gbc.gridx = 0;
-        gbc.gridy = 1;
+        gbc.gridy = 2;
         gbc.gridwidth = 1;
         gbc.gridheight = 1;
         gbc.weightx = 0;
