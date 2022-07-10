e6272d073305cf084fd8715579bf8441c2bdf257
btdun2002 <54074236+btdun2002@users.noreply.github.com>
2022-07-10 16:45:33 +0700

    Change line color

--- src/view/Util/Palette.java
+++ src/view/Util/Palette.java
@@ -7,4 +7,15 @@ public class Palette {
     public static Color HUST_RED = new Color(205, 22, 39);
     public static Color WHITE = new Color(255, 255, 255);
     public static Color BLACK = new Color(0, 0, 0);
+    public static Color LIGHT_GRAY = new Color(229, 229, 229);
+    public static Color[] NINE_LINE_COLOR = { Color.decode("#9e0142"),
+            Color.decode("#d53e4f"),
+            Color.decode("#f46d43"),
+            Color.decode("#fdae61"),
+            Color.decode("#fee08b"),
+            Color.decode("#abdda4"),
+            Color.decode("#66c2a5"),
+            Color.decode("#3288bd"),
+            Color.decode("#5e4fa2") };
+
 }
